c5f4dcedbb148c0b500d39f8387e4488af669c4c
Atish Patra <atishp@rivosinc.com>
2024-07-03 02:21:29 -0700

    Add SMMC driver package based.
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

---
+++ drivers/irqchip/irq-riscv-smmc.c
@@ -0,0 +1,302 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2024 Rivos Inc.
+ */
+
+#include "asm-generic/errno-base.h"
+#include "linux/list.h"
+#include <linux/acpi.h>
+#include <linux/bitfield.h>
+#include <linux/irqchip/riscv-imsic.h>
+#include <linux/irqdomain.h>
+#include <linux/irq.h>
+#include <linux/module.h>
+#include <linux/msi.h>
+#include <linux/platform_device.h>
+#include <linux/printk.h>
+#include <acpi/actypes.h>
+
+struct smmc_dev_cfg {
+	u64 gsino;
+	u64 addr_low;
+	u64 addr_hi;
+	u64 msidata;
+	u64 enable;
+};
+
+struct smmc_dev_res {
+	struct list_head list;
+	struct smmc_dev_cfg *cfg;
+	void __iomem	*regs;
+};
+
+struct smmc_data {
+	struct device	*dev;
+	u32             gsi_base;
+	u32             nr_irqs;
+	u32             id;
+	struct list_head dev_res_list;
+};
+
+static struct smmc_dev_res *smmc_find_dres_for_gsi(struct smmc_data *data, int hwirq)
+{
+	struct smmc_dev_res *devres = NULL;
+	int gsi = data->gsi_base + hwirq;
+
+	pr_err("data->gsi_base %d hwirq %d gsi %d\n", data->gsi_base, hwirq, gsi);
+	list_for_each_entry(devres, &data->dev_res_list, list) {
+		pr_err("gsino %d regs %lx ah %lx\n", devres->cfg->gsino, devres->regs, devres->cfg->addr_hi);
+		if (gsi == devres->cfg->gsino)
+			return devres;
+	}
+
+	if (!devres)
+		pr_err("can't find a dev resource for gsi %d\n", hwirq);
+
+	return NULL;
+}
+
+static void smmc_msi_irq_unmask(struct irq_data *d)
+{
+	struct smmc_data *data = irq_data_get_irq_chip_data(d);
+	struct smmc_dev_res *devres = smmc_find_dres_for_gsi(data, d->hwirq);
+
+	if (!devres)
+		return;
+
+	irq_chip_unmask_parent(d);
+	pr_err("%s: In \n", __func__);
+	writel(1, devres->regs + devres->cfg->enable);
+}
+
+static void smmc_msi_irq_mask(struct irq_data *d)
+{
+	struct smmc_data *data = irq_data_get_irq_chip_data(d);
+	struct smmc_dev_res *devres = smmc_find_dres_for_gsi(data, d->hwirq);
+
+	if (!devres)
+		return;
+
+	writel(0, devres->regs + devres->cfg->enable);
+	irq_chip_mask_parent(d);
+}
+
+static int smmc_msi_irq_set_type(struct irq_data *d, unsigned int type)
+{
+    pr_err("%s: In type %d \n", __func__, type);
+
+    return 0;
+}
+
+static void smmc_msi_write_msg(struct irq_data *d, struct msi_msg *msg)
+{
+	struct smmc_data *data = irq_data_get_irq_chip_data(d);
+	struct smmc_dev_res *devres = smmc_find_dres_for_gsi(data, d->hwirq);
+
+	if (!devres)
+		return;
+
+	//TODO: Remove this
+	if (d->hwirq == 1)
+		return;
+
+	pr_err("%s: In d->irq %d hwirq %d msg_hi %lx msg_low %lx data %lx\n",
+	   __func__, d->irq, d->hwirq, msg->address_hi, msg->address_lo, msg->data);
+
+	writel(msg->address_lo, devres->regs + devres->cfg->addr_low);
+	writel(msg->address_hi, devres->regs + devres->cfg->addr_hi);
+	writel(msg->data, devres->regs + devres->cfg->msidata);
+	pr_err("%s: regs.0 %x\n", __func__, readl(devres->regs + devres->cfg->addr_low));
+	pr_err("%s: regs.1 %x\n", __func__, readl(devres->regs + devres->cfg->addr_hi));
+	pr_err("%s: regs.2 %x\n", __func__, readl(devres->regs + devres->cfg->msidata));
+	pr_err("%s: regs.3 %x\n", __func__, readl(devres->regs + devres->cfg->enable));
+}
+
+static void smmc_msi_set_desc(msi_alloc_info_t *arg, struct msi_desc *desc)
+{
+	arg->desc = desc;
+	arg->hwirq = (u32)desc->data.icookie.value;
+}
+
+static int smmc_msi_irqdomain_translate(struct irq_domain *d, struct irq_fwspec *fwspec,
+			       unsigned long *hwirq, unsigned int *type)
+{
+	struct msi_domain_info *info = d->host_data;
+	struct smmc_data *data = info->data;
+
+    	if (WARN_ON(fwspec->param_count < 2))
+		return -EINVAL;
+	if (WARN_ON(!fwspec->param[0]))
+		return -EINVAL;
+
+	/* For DT, gsi_base is always zero. */
+	*hwirq = fwspec->param[0] - data->gsi_base;
+	*type = fwspec->param[1] & IRQ_TYPE_SENSE_MASK;
+
+	WARN_ON(*type == IRQ_TYPE_NONE);
+
+	return 0;
+}
+
+static const struct msi_domain_template smmc_msi_template = {
+	.chip = {
+		.name			= "SMMC-MSI",
+		.irq_mask		= smmc_msi_irq_mask,
+		.irq_unmask		= smmc_msi_irq_unmask,
+		.irq_set_type	= smmc_msi_irq_set_type,
+		//.irq_eoi		= aplic_msi_irq_eoi,
+#ifdef CONFIG_SMP
+		.irq_set_affinity	= irq_chip_set_affinity_parent,
+#endif
+		.irq_write_msi_msg	= smmc_msi_write_msg,
+		.flags			= IRQCHIP_SET_TYPE_MASKED |
+					  IRQCHIP_SKIP_SET_WAKE |
+					  IRQCHIP_MASK_ON_SUSPEND,
+	},
+
+	.ops = {
+		.set_desc		= smmc_msi_set_desc,
+		.msi_translate		= smmc_msi_irqdomain_translate,
+	},
+
+	.info = {
+		.bus_token		= DOMAIN_BUS_WIRED_TO_MSI, //TODO: Check
+		.flags			= MSI_FLAG_USE_DEV_FWNODE,
+		//.handler		= handle_fasteoi_irq,
+		//.handler_name		= "fasteoi",
+	},
+};
+
+static int smmc_parse_package_resource(struct platform_device *pdev, struct smmc_data *data)
+{
+	struct device *dev = &pdev->dev;
+	int result = -EFAULT;
+	acpi_status status = AE_OK;
+	struct acpi_buffer buffer = {ACPI_ALLOCATE_BUFFER, NULL};
+	union acpi_object *smmc_obj = NULL;
+	struct acpi_buffer format = {sizeof("NNNNN"), "NNNNN"};
+	struct acpi_buffer state = {0, NULL};
+	struct smmc_dev_res *res;
+	int i;
+
+	struct acpi_handle *handle = ACPI_HANDLE_FWNODE(dev->fwnode);
+
+	status = acpi_evaluate_object_typed(handle, "CFGN", NULL,
+					    &buffer, ACPI_TYPE_PACKAGE);
+	if (status == AE_NOT_FOUND)
+		return 0;
+	if (ACPI_FAILURE(status))
+		return -ENODEV;
+
+	smmc_obj = buffer.pointer;
+	if (!smmc_obj || smmc_obj->package.count != 2) {
+		pr_err("Invalid SMMC data\n");
+		goto end;
+	}
+
+	pr_err("%s: package count %d\n", __func__, smmc_obj->package.count);
+
+	for (i = 0; i < smmc_obj->package.count; i++) {
+		res = devm_kzalloc(dev, sizeof(*res), GFP_KERNEL);
+		if (!res)
+			goto end_nomem;
+		res->cfg = devm_kzalloc(dev, sizeof(struct smmc_dev_cfg), GFP_KERNEL);
+		if (!res->cfg)
+			goto end_nomem;
+
+		res->regs = devm_platform_ioremap_resource(pdev, i);
+		if (IS_ERR(res->regs)) {
+			result = PTR_ERR(res->regs);
+			//TODO: Free up the list/memory for cfg
+			goto end;
+		}
+
+		state.length = sizeof(struct smmc_dev_cfg);
+		state.pointer = res->cfg;
+
+		status = acpi_extract_package(&(smmc_obj->package.elements[i]),
+				      &format, &state);
+		if (ACPI_FAILURE(status)) {
+			pr_err("Invalid SMMC CFG %d\n", status);
+			goto end;
+		}
+
+		pr_err("%s: smmc_obj data package i %d: %d %lx %lx \n", __func__, i, res->cfg->gsino, res->cfg->addr_low, res->cfg->addr_hi);
+		list_add(&res->list, &data->dev_res_list);
+	}
+	result = 0;
+
+end_nomem:
+	result = -ENOMEM;
+end:
+	kfree(buffer.pointer);
+	return result;
+}
+
+static int smmc_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	int rc = 0;
+    	const struct imsic_global_config *imsic_global;
+	struct irq_domain *msi_domain;
+	struct smmc_data *data;
+
+	/* SMMC device is only valid for ACPI supported platforms */
+	if (acpi_disabled)
+		return -ENODEV;
+
+	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+
+	data->dev = dev;
+	INIT_LIST_HEAD(&data->dev_res_list);
+
+    	rc = riscv_acpi_get_gsi_info(dev->fwnode, &data->gsi_base, &data->id,
+					     &data->nr_irqs, NULL);
+	if (rc) {
+		dev_err(dev, "failed to find GSI mapping\n");
+		return rc;
+	}
+
+	smmc_parse_package_resource(pdev, data);
+	//TODO: Setup any initial state of the device and enable MSI delivery
+
+	imsic_global = imsic_get_global_config();
+	if (!imsic_global) {
+		dev_err(dev, "IMSIC global config not found\n");
+		return -ENODEV;
+	}
+
+	if (!dev_get_msi_domain(dev)) {
+		msi_domain = irq_find_matching_fwnode(imsic_acpi_get_fwnode(dev),
+							DOMAIN_BUS_PLATFORM_MSI);
+			if (msi_domain)
+				dev_set_msi_domain(dev, msi_domain);
+	}
+
+	if (!msi_create_device_irq_domain(dev, MSI_DEFAULT_DOMAIN, &smmc_msi_template,
+						data->nr_irqs + 1, data, data)) {
+		dev_err(dev, "failed to create MSI irq domain\n");
+		return -ENOMEM;
+	}
+
+	acpi_dev_clear_dependencies(ACPI_COMPANION(dev));
+
+	return rc;
+}
+
+static const struct acpi_device_id smmc_acpi_match[] = {
+	{ "RSCV0005", 0 },
+	{}
+};
+MODULE_DEVICE_TABLE(acpi, smmc_acpi_match);
+
+static struct platform_driver smmc_driver = {
+	.driver = {
+		.name		= "riscv-smmc",
+		.acpi_match_table = ACPI_PTR(smmc_acpi_match),
+	},
+	.probe = smmc_probe,
+};
+builtin_platform_driver(smmc_driver);
