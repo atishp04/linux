e34aff34340ce254272dafe12443cea579f15442
Charlie Jenkins <charlie@rivosinc.com>
2024-08-26 13:37:45 -0700

    RISC-V: Perf: Skip PMU SBI extension when not implemented
    When the PMU SBI extension is not implemented, sbi_v2_available should
    not be set to true. The SBI implementation for counter config matching
    and firmware counter read  should also be skipped when the SBI extension
    is not implemented.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>

--- drivers/perf/riscv_pmu_dev.c
+++ drivers/perf/riscv_pmu_dev.c
@@ -362,18 +362,31 @@ static void rvpmu_sbi_check_event(struct sbi_pmu_event_data *edata)
 	}
 }
 
-static void rvpmu_sbi_check_std_events(struct work_struct *work)
+static void rvpmu_deleg_check_event(struct sbi_pmu_event_data *edata)
+{
+	DO_ONCE_LITE_IF(1, pr_err, "Find and configure matching counter not implemented for smcdeleg\n");
+}
+
+static void rvpmu_check_event(struct sbi_pmu_event_data *edata)
+{
+	if (riscv_pmu_cdeleg_available())
+		rvpmu_deleg_check_event(edata);
+	else
+		rvpmu_sbi_check_event(edata);
+}
+
+static void rvpmu_check_std_events(struct work_struct *work)
 {
 	for (int i = 0; i < ARRAY_SIZE(pmu_hw_event_map); i++)
-		rvpmu_sbi_check_event(&pmu_hw_event_map[i]);
+		rvpmu_check_event(&pmu_hw_event_map[i]);
 
 	for (int i = 0; i < ARRAY_SIZE(pmu_cache_event_map); i++)
 		for (int j = 0; j < ARRAY_SIZE(pmu_cache_event_map[i]); j++)
 			for (int k = 0; k < ARRAY_SIZE(pmu_cache_event_map[i][j]); k++)
-				rvpmu_sbi_check_event(&pmu_cache_event_map[i][j][k]);
+				rvpmu_check_event(&pmu_cache_event_map[i][j][k]);
 }
 
-static DECLARE_WORK(check_std_events_work, rvpmu_sbi_check_std_events);
+static DECLARE_WORK(check_std_events_work, rvpmu_check_std_events);
 
 static ssize_t rvpmu_format_show(struct device *dev,
 				 struct device_attribute *attr, char *buf)
@@ -502,6 +515,9 @@ static int rvpmu_sbi_ctr_get_idx(struct perf_event *event)
 
 	cflags = rvpmu_sbi_get_filter_flags(event);
 
+	if (!riscv_pmu_sbi_available())
+		return -ENOENT;
+
 	/*
 	 * In legacy mode, we have to force the fixed counters for those events
 	 * but not in the user access mode as we want to use the other counters
@@ -515,10 +531,9 @@ static int rvpmu_sbi_ctr_get_idx(struct perf_event *event)
 			cflags |= SBI_PMU_CFG_FLAG_SKIP_MATCH;
 			ctr_mask = BIT(CSR_INSTRET - CSR_CYCLE);
 		}
-	}
-
-	if (pmu_sbi_is_fw_event(event) && cdeleg_available)
+	} else if (pmu_sbi_is_fw_event(event) && cdeleg_available) {
 		ctr_mask = firmware_cmask;
+	}
 
 	/* retrieve the available counter index */
 #if defined(CONFIG_32BIT)
@@ -767,7 +782,7 @@ static u64 rvpmu_ctr_read(struct perf_event *event)
 		return val;
 	}
 
-	if (pmu_sbi_is_fw_event(event)) {
+	if (pmu_sbi_is_fw_event(event) && riscv_pmu_sbi_available()) {
 		ret = sbi_ecall(SBI_EXT_PMU, SBI_EXT_PMU_COUNTER_FW_READ,
 				hwc->idx, 0, 0, 0, 0, 0);
 		if (ret.error)
@@ -1867,14 +1882,16 @@ static int __init rvpmu_devinit(void)
 	int ret;
 	struct platform_device *pdev;
 
-	if (sbi_spec_version >= sbi_mk_version(0, 3) &&
-	    sbi_probe_extension(SBI_EXT_PMU)) {
-		static_branch_enable(&riscv_pmu_sbi_available);
-		sbi_available = true;
+	if (sbi_probe_extension(SBI_EXT_PMU)) {
+		if (sbi_spec_version >= sbi_mk_version(0, 3)) {
+			static_branch_enable(&riscv_pmu_sbi_available);
+			sbi_available = true;
+		}
+
+		if (sbi_spec_version >= sbi_mk_version(2, 0))
+			sbi_v2_available = true;
 	}
 
-	if (sbi_spec_version >= sbi_mk_version(2, 0))
-		sbi_v2_available = true;
 	/*
 	 * We need all three extensions to be present to access the counters
 	 * in S-mode via Supervisor Counter delegation.
